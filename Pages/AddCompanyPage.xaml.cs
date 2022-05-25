12b29528332ae5eab827d2e8ea2019b1fe4538a2
VikashRajpurohit <98598930+VikashRajpurohit@users.noreply.github.com>
2022-05-25 13:44:41 +0530

    abcd

--- Pages/AddCompanyPage.xaml.cs
+++ Pages/AddCompanyPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,66 @@ namespace ShreeGovardhanTextilesSystem.Pages
     /// </summary>
     public partial class AddCompanyPage : Page
     {
+
+        String name, gst, broker, master, address, del;
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ADMIN\Source\Repos\ShreeGovardhanTextilesSystem\sgtdb.mdf;Integrated Security=True");
+
+
         public AddCompanyPage()
         {
             InitializeComponent();
+            loaddata();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            name = txtname.Text;
+            gst = txtgst.Text;
+            broker = txtbroker.Text;
+            master = txtmst.Text;
+            address = txtadd.Text;
+            del = txtdel.Text;
+
+            SqlCommand cmd = new SqlCommand("insert into tbl_company (name,gst,delivery,broker,master,address)" +
+                " values (@name,@gst,@del,@broker,@master,@address) ", con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@gst", gst);
+            cmd.Parameters.AddWithValue("@del", del);
+            cmd.Parameters.AddWithValue("@broker", broker);
+            cmd.Parameters.AddWithValue("@master", master);
+            cmd.Parameters.AddWithValue("@address", address);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            clearData();
+            loaddata();
+
+        }
+
+        //to clear the text feilds
+        public void clearData()
+        {
+            txtname.Clear();
+            txtadd.Clear();
+            txtbroker.Clear();
+            txtdel.Clear();
+            txtgst.Clear();
+            txtmst.Clear();
+        }
+
+
+        public void loaddata()
+        {
+            SqlCommand cmd = new SqlCommand("select * from tbl_company", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            datagrid.ItemsSource = dt.DefaultView;
+        }
+
         }
     }
-}
--- Pages/ReportPage.xaml.cs
+++ Pages/ReportPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,18 +22,23 @@ namespace ShreeGovardhanTextilesSystem.Pages
     /// </summary>
     public partial class ReportPage : Page
     {
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ADMIN\Source\Repos\ShreeGovardhanTextilesSystem\sgtdb.mdf;Integrated Security=True");
+        String[] dataset = {"Stock","Bim Stock"};
         public ReportPage()
         {
             InitializeComponent();
+            dd.ItemsSource = dataset;
         }
 
 
         private void Show_Click(object sender, RoutedEventArgs e)
 
         {
-
-            MyPopup.IsOpen = true;
-
+           
+            //NavigationService.Navigate(new Uri(@"C:\Users\ADMIN\Source\Repos\ShreeGovardhanTextilesSystem\Pages\ProductionPage.xaml", UriKind.Relative));
+            //ProductionPage productionPage = new ProductionPage();
+            //this.Content = productionPage;
         }
 
 
@@ -44,6 +51,24 @@ namespace ShreeGovardhanTextilesSystem.Pages
 
         }
 
+        private void cmbCountryList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            loaddata(e.ToString());
+        }
+
+        public void loaddata(String i) {
+            if (i == "Stock")
+            {
+                SqlCommand cmd = new SqlCommand("select * from tbl_purchases", con);
+                DataTable dt = new DataTable();
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                dt.Load(sdr);
+                con.Close();
+                datagrid.ItemsSource = dt.DefaultView;
+            }
+        }
+             
     }
 
 }
