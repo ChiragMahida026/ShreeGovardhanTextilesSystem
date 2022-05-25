efc19159fb5b01fc3851116d05cd4b9ec8efbdac
VikashRajpurohit <98598930+VikashRajpurohit@users.noreply.github.com>
2022-05-25 17:02:29 +0530

    commit #5:02

--- Pages/BoxUsed.xaml.cs
+++ Pages/BoxUsed.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
+
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -23,6 +24,7 @@ namespace ShreeGovardhanTextilesSystem.Pages
 
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ADMIN\Source\Repos\ShreeGovardhanTextilesSystem\sgtdb.mdf;Integrated Security=True");
         String serial, dateused;
+        String[] code;
 
         public BoxUsed()
         {
@@ -33,12 +35,50 @@ namespace ShreeGovardhanTextilesSystem.Pages
             txtdaterec.Text = now.ToString("d");
             dateused = txtdaterec.Text;
 
+            SqlCommand cmd = new SqlCommand("select serial from tbl_purchases", con);
+
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            List<string> numbersList = new List<string>();
+
+            while (sdr.Read())
+            {
+                numbersList.Add(Convert.ToString(sdr["serial"]));
+            }
+            txtserial.ItemsSource = numbersList;
+            con.Close();
+            loaddata();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            serial = txtserial.Text;
+            try
+            {
+                serial = txtserial.Text;
 
+                SqlCommand cmd = new SqlCommand("update tbl_purchases set date_used = @dateused where serial = @serial ", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@serial", serial);
+                cmd.Parameters.AddWithValue("@dateused", dateused);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                loaddata();
+            }
+            catch (SqlException err) { MessageBoxResult result = MessageBox.Show(serial + " " + dateused + "  " + err.ToString()); }
+        }
+
+        public void loaddata()
+        {
+            SqlCommand cmd = new SqlCommand("select * from tbl_purchases where date_used is not null order by date_used desc", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+
+            datagrid.ItemsSource = dt.DefaultView;
         }
     }
 }
